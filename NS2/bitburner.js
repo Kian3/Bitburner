2d2a8e0f8463473a35f00a39ea1905d7be951b5d
Kian <87499307+Kian3@users.noreply.github.com>
2022-03-23 13:55:03 +0000

    Update bitburner.js

--- NS2/bitburner.js
+++ NS2/bitburner.js
@@ -1,3 +1,4 @@
+// This creates a small window in game, which opens a new game of Bitburner, pretty pointless.
 /** @param {NS} ns **/
 export async function main(ns) {
 ns.alert(`<iframe width="640" height="480" src="https://danielyxie.github.io/bitburner/"></iframe>`);
