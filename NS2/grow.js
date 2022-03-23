a5e291a6df1eff47a81bd53f5584702318f1991d
Kian <87499307+Kian3@users.noreply.github.com>
2022-03-23 13:56:24 +0000

    Update grow.js

--- NS2/grow.js
+++ NS2/grow.js
@@ -1,3 +1,4 @@
+// Simple script to run grow on the server stated in the argument. Mainly used by MasterHack.js
 /** @param {NS} ns **/
 export async function main(ns) {
 	await ns.grow(ns.args[0]);
